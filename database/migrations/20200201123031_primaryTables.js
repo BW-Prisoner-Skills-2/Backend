3aad242d666bc067f36921807e51bb40f2a9890a
yutveg <kodi.0n31l@gmail.com>
2020-02-01 15:02:27 -0800

    migrations table added

--- database/migrations/20200201123031_primaryTables.js
+++ database/migrations/20200201123031_primaryTables.js
@@ -25,7 +25,67 @@ exports.up = function(knex) {
         .unique();
       table.integer("zipcode", 12).notNullable();
       table.integer("prisoner_quantity", 10);
+    })
+    .createTable("prisoners", table => {
+      table.increments();
+      table
+        .string("name", 50)
+        .notNullable()
+        .index();
+      table.boolean("can leave").notNullable();
+    })
+    .createTable("prison_prisoners", table => {
+      table.increments();
+      table
+        .integer("prison_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("prisons")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table
+        .integer("prisoner_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("prisoners")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("prisoner_skills", table => {
+      table.increments();
+      table
+        .integer("prisoner_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("prisoners")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table.string("description", 120).notNullable();
+    })
+    .createTable("prisoner_experience", table => {
+      table.increments();
+      table
+        .integer("prisoner_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("prisoners")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table.string("description", 250).notNullable();
+      table.string("date", 30).notNullable();
     });
 };
 
-exports.down = function(knex) {};
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("prisoner_experience")
+    .dropTableIfExists("prisoner_skills")
+    .dropTableIfExists("prison_prisoners")
+    .dropTableIfExists("prisoners")
+    .dropTableIfExists("prisons")
+    .dropTableIfExists("admins");
+};
