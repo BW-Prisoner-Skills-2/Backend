be1351a75dc48ab6ba6ec82e004e6361079a79ee
yutveg <kodi.0n31l@gmail.com>
2020-02-02 12:05:58 -0800

    merge params is true and i am a happy man

--- prisoners/prisoner-router.js
+++ prisoners/prisoner-router.js
@@ -1,4 +1,4 @@
-const router = require("express").Router();
+const router = require("express").Router({ mergeParams: true });
 const Prisoners = require("./prisoner-model.js");
 
 router.get("/", (req, res) => {
