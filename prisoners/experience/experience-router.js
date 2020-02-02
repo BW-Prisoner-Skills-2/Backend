8084a634cd71aa88626baf51470b668ef0052442
yutveg <kodi.0n31l@gmail.com>
2020-02-02 13:07:46 -0800

    endpoints all functional need to tweak returns on a few

--- prisoners/experience/experience-router.js
+++ prisoners/experience/experience-router.js
@@ -1,8 +1,47 @@
 const router = require("express").Router({ mergeParams: true });
 const Experience = require("./experience-model.js");
+const { validateAdmin } = require("../../auth/config/auth-middleware.js");
 
-router.post("/", (req, res) => {
-  Experience.add({ ...req.body, prisoner_id: req.prisonerid });
+router.post("/", validateAdmin, validatePost, (req, res) => {
+  Experience.add({ ...req.body, prisoner_id: req.params.prisonerid })
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(error => {
+      res.status(500).json(error.message);
+    });
 });
+
+router.put("/:experienceid", validateAdmin, (req, res) => {
+  Experience.update(req.body, req.params.experienceid)
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(error => {
+      res.status(500).json(error.message);
+    });
+});
+
+router.delete("/:experienceid", validateAdmin, (req, res) => {
+  Experience.remove(req.params.experienceid)
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(error => {
+      res.status(500).json(error.message);
+    });
+});
+
+//Middleware to check post
+function validatePost(req, res, next) {
+  if (req.body && req.body.description && req.body.date) {
+    next();
+  } else {
+    res.status(400).json({
+      error:
+        "Missing necessary information. Ensure you are sending a description AND a date- both are strings/text."
+    });
+  }
+}
 
 module.exports = router;
--- prisoners/skills/skills-router.js
+++ prisoners/skills/skills-router.js
@@ -1,3 +1,50 @@
 const router = require("express").Router({ mergeParams: true });
+const Skills = require("./skills-model.js");
+const { validateAdmin } = require("../../auth/config/auth-middleware.js");
+
+router.post("/", validateAdmin, validatePost, (req, res) => {
+  Skills.add({
+    ...req.body,
+    prisoner_id: req.params.prisonerid
+  })
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(error => {
+      res.status(500).json(error.message);
+    });
+});
+
+router.put("/:skillid", validateAdmin, (req, res) => {
+  Skills.update(req.body, req.params.skillid)
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(error => {
+      res.status(500).json(error.message);
+    });
+});
+
+router.delete("/:skillid", validateAdmin, (req, res) => {
+  Skills.remove(req.params.skillid)
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(error => {
+      res.status(500).json(error.message);
+    });
+});
+
+//Middleware to check post
+function validatePost(req, res, next) {
+  if (req.body && req.body.description) {
+    next();
+  } else {
+    res.status(400).json({
+      error:
+        "Missing necessary information. Ensure you are sending a description- it is a string/text."
+    });
+  }
+}
 
 module.exports = router;
